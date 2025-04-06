cc97b4d61c1300f4f792fa1b0139027cbb4952fe
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-04-06 17:19:27 +0200

    setup and start the java client

---
+++ java-client/src/main/java/org/example/scraping/Article.java
@@ -0,0 +1,11 @@
+package org.example.scraping;
+
+public class Article {
+    public String time;
+    public String title;
+
+    public Article(String time, String title) {
+        this.time = time;
+        this.title = title;
+    }
+}
---
+++ java-client/src/test/java/ResolveDayTest.java
@@ -0,0 +1,30 @@
+import org.example.scraping.DayArticles;
+import java.time.LocalDate;
+import java.util.Locale;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ResolveDayTest {
+    @Test
+    public void testResolveDayNameToDateOneWeekBefore() {
+        LocalDate today = LocalDate.of(2025, 4, 4); // Samedi 5 avril
+        String dayName = "Dimanche";
+        LocalDate expectedDate = LocalDate.of(2025, 3, 30); // Dimanche 30 mars
+
+        LocalDate result = DayArticles.resolveDayNameToDate(dayName, today);
+        assertEquals(expectedDate, result);
+    }
+
+    @Test
+    public void testResolveDayNameToDateSameWeek() {
+        LocalDate today = LocalDate.of(2025, 4, 4); // Samedi 5 avril
+        String dayName = "Vendredi";
+        LocalDate expectedDate = LocalDate.of(2025, 4, 4); // Vendredi 4 avril
+
+        LocalDate result = DayArticles.resolveDayNameToDate(dayName, today);
+        assertEquals(expectedDate, result);
+    }
+}
