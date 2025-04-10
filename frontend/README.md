56406fe7425483673a849ffe9028d312610461e7
Makav77 <briceminet@hotmail.fr>
2025-04-10 16:24:51 +0200

    feat: add form fields

--- frontend/README.md
+++ frontend/README.md
@@ -4,6 +4,7 @@ Dans le dossier frontend/
 => npm install @types/react-router-dom
 => npm install i18next-http-backend
 => npm install tailwindcss
+=> npm install uuid
 
 
 Avant chaque commit :
@@ -14,13 +15,8 @@ npm run format
 
 Features:
 
-Login page :
+Register page :
 
-mail and password field
-show/hide password button + toggle visibility with space bar
-forgot password link for reset password (to do)
-remember me checkbox for changes the token validity time (to do)
-registration link that redirects to the account creation page (to do)
-simple error handling => invalid credentials / missing credentials / unknow error (fields shake and border turns red)
-spin animation on login button when submit
-add internationalization (en/fr)
+firstname, lastname, email, password and date of birth fields
+register and cancel button (cancel button to return to the login page)
+
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -1,6 +1,95 @@
+import { useState } from "react";
+
+interface registerCredentials {
+    id: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string,
+    dateOfBirth: string
+};
+
 function Signup() {
+    const [formData, setFormData] = useState<registerCredentials>({
+        id: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        dateOfBirth: ""
+    });
+
     return (
-        <p>Hello World</p>
+        <div className="bg-white w-[384px] mx-auto mt-20 rounded-[2vw] text-center p-6">
+            <h1 className="text-black font-bold text-5xl mt-10 mb-2">
+                title
+            </h1>
+
+            <form
+                className="flex flex-col gap-5 w-4/5 mx-auto"
+            >
+                <input
+                    name="firstname"
+                    type="text"
+                    autoComplete="off"
+                    className="bg-[#F2EBDC] text-black border-2 rounded-xl h-8 pl-5 hover:border-black"
+                    value={ formData.firstname || "" }
+                    placeholder="Firstname"
+                />
+
+                <input
+                    name="lastname"
+                    type="text"
+                    autoComplete="off"
+                    className="bg-[#F2EBDC] text-black border-2 rounded-xl h-8 pl-5 hover:border-black"
+                    value={ formData.lastname || "" }
+                    placeholder="Lastname"
+                />
+
+                <input
+                    name="email"
+                    type="email"
+                    autoComplete="off"
+                    className="bg-[#F2EBDC] text-black border-2 rounded-xl h-8 pl-5 hover:border-black"
+                    value={ formData.email || "" }
+                    placeholder="Email"
+                />
+
+                <input
+                    name="password"
+                    type="password"
+                    autoComplete="off"
+                    className="bg-[#F2EBDC] text-black border-2 rounded-xl h-8 pl-5 hover:border-black"
+                    value={ formData.password || "" }
+                    placeholder="Password"
+                />
+
+                <input
+                    name="dateOfBirth"
+                    type="date"
+                    autoComplete="off"
+                    className="bg-[#F2EBDC] text-black border-2 rounded-xl h-8 pl-5 hover:border-black"
+                    value={ formData.dateOfBirth || "" }
+                    placeholder="Firstname"
+                />
+
+                <div className="flex gap-5">
+                    <button
+                        type="button"
+                        className="border-none bg-[#488ACF] text-1xl font-bold w-1/3 mx-auto mt-7 mb-2 p-2 text-white rounded-lg cursor-pointer transition-all duration-300 flex justify-center items-center"
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        type="submit"
+                        className="border-none bg-[#488ACF] text-1xl font-bold w-2/3 mx-auto mt-7 mb-2 p-2 text-white rounded-lg cursor-pointer transition-all duration-300 flex justify-center items-center"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
     );
 }
 
