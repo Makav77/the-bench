aabf468f051c2d20f37dadd3f37a611c539626ec
Makav77 <briceminet@hotmail.fr>
2025-04-17 20:51:13 +0200

    feat(FOOTER): add signature footer is present test

---
+++ frontend/src/__tests__/Footer/Footer.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Footer from "../../components/Footer/Footer";
+
+describe("Signature", () => {
+    test("Footer signature is present", () => {
+        render(<Footer />);
+        expect(screen.getByText("The Bench © 2025")).toBeInTheDocument();
+    })
+})
