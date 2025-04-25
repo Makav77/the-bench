954df76aeaaf890ecb065159ec0fb38b830a6d7b
Makav77 <briceminet@hotmail.fr>
2025-04-25 10:13:39 +0200

    feat: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, { logger: ["error", "warn", "log", "debug", "verbose"] });
     const port = 3000;
 
+    app.enableCors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    });
+
     await app.listen(port, () => {
         console.log(`Server running on http://localhost:${port}`);
     });
