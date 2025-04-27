10969ceb1a9b4ac174a5f4ba676f2cdf85772737
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-04-27 16:27:41 +0200

    feat: add javafx and launch the interface in main

--- java-client/src/main/java/org/example/Main.java
+++ java-client/src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
+import org.example.dao.DayArticlesDAO;
+import org.example.scraping.DayArticles;
+import org.example.scraping.DayArticlesUtils;
 import org.example.scraping.Scraper;
 import org.example.db.DatabaseManager;
+import java.util.List;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("The bench");
+        stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(700);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        Scraper.getNewsParis();
+        launch(args);
     }
 }
