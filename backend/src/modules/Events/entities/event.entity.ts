79bf8715444a03c2e8fa58d05b56adaf253b2330
Makav77 <briceminet@hotmail.fr>
2025-04-29 17:51:00 +0200

    feat: add event entity

---
+++ backend/src/modules/Events/entities/event.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+import { User } from "src/modules/Users/entities/user.entity";
+
+@Entity({ name: "events" })
+export class Event {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column({ type: "timestamptz"})
+    startDate: Date;
+
+    @Column({ type: "timestamptz"})
+    endDate: Date;
+
+    @CreateDateColumn({ type: "timestamptz" })
+    createdDate: Date;
+
+    @Column()
+    place: string;
+
+    @Column({ type: "int", nullable: true })
+    maxNumberOfParticipants?: number;
+
+    @Column()
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.eventsCreated, { nullable: false })
+    author: User;
+
+    @ManyToMany(() => User, (user) => user.eventsParticipating, { nullable: true })
+    @JoinTable({
+        name: "event_participants",
+        joinColumn: { name: "event_id", referencedColumnName: "id" },
+        inverseJoinColumn: { name: "user_id", referencedColumnName: "id" },
+    })
+    participantsList?: User[];
+}
