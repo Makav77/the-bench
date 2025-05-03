5f10781f49bea441b7724de56a7839b31c099cc9
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-05-04 00:24:33 +0200

    feat: add plugins controller

---
+++ java-client/src/main/java/org/example/controller/PluginsController.java
@@ -0,0 +1,10 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+
+public class PluginsController {
+    @FXML
+    private void initialize() {
+        System.out.println("Vue Plugins chargée !");
+    }
+}
