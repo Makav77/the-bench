6990551cf0b02828b74243e269d8db321d6047b0
Makav77 <briceminet@hotmail.fr>
2025-05-07 18:37:41 +0200

    feat(backend): add create, update and delete item in market.controller

--- backend/src/modules/Market/market.controller.ts
+++ backend/src/modules/Market/market.controller.ts
@@ -5,6 +5,7 @@ import { UpdateMarketItemDTO } from './dto/update-market-item.dto';
 import { JwtAuthGuard } from '../Auth/guards/jwt-auth.guard';
 import { Request } from 'express';
 import { MarketItem } from './entities/market.entity';
+import { User } from '../Users/entities/user.entity';
 
 @Controller("market")
 export class MarketController {
@@ -23,5 +24,36 @@ export class MarketController {
     @Get(":id")
     async findOneItem(@Param("id") id: string): Promise<MarketItem> {
         return this.marketService.findOneItem(id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createItem(
+        @Body() createItemDTO: CreateMarketItemDTO,
+        @Req() req: Request,
+    ): Promise<MarketItem> {
+        const user = req.user as User;
+        return this.marketService.createItem(createItemDTO, user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch(":id")
+    async updateItem(
+        @Param("id") id: string,
+        @Body() updateItemDTO: UpdateMarketItemDTO,
+        @Req() req: Request,
+    ): Promise<MarketItem> {
+        const user = req.user as User;
+        return this.marketService.updateItem(id, updateItemDTO, user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(":id")
+    async removeItem (
+        @Param("id") id: string,
+        @Req() req: Request,
+    ): Promise<void> {
+        const user = req.user as User;
+        return this.marketService.removeItem(id, user);
     }
 }
