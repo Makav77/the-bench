0dab8004f1a4579fee28ec8fd83b2bb7e0269470
Makav77 <briceminet@hotmail.fr>
2025-05-07 21:52:12 +0200

    feat(backend): initialize Multer in main and add route for upload images with interceptor

--- backend/src/modules/Market/market.controller.ts
+++ backend/src/modules/Market/market.controller.ts
@@ -1,4 +1,8 @@
 import { Controller, Get, Param, Query, Post, Body, Patch, Delete, UseGuards, Req, DefaultValuePipe, ParseIntPipe } from '@nestjs/common';
+import { UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from "multer";
+import { extname } from 'path';
 import { MarketService } from './market.service';
 import { CreateMarketItemDTO } from './dto/create-market-item.dto';
 import { UpdateMarketItemDTO } from './dto/update-market-item.dto';
@@ -34,6 +38,41 @@ export class MarketController {
     ): Promise<MarketItem> {
         const user = req.user as User;
         return this.marketService.createItem(createItemDTO, user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(":id/images")
+    @UseInterceptors(
+        FilesInterceptor("images", 5, {
+            storage: diskStorage({
+                destination: "./uploads/market",
+                filename: (_req, file, callback) => {
+                    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+                    const ext = extname(file.originalname);
+                    callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+                },
+            }),
+            fileFilter: (_req, file, callback) => {
+                if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+                    callback(null, true);
+                } else {
+                    callback(new Error("Unsupported file type."), false);
+                }
+            },
+            limits: { fileSize: 2 * 1024 * 1024 },
+        }),
+    )
+
+    async uploadImages(
+        @Param("id") id: string,
+        @UploadedFiles() files: Array<Express.Multer.File>,
+        @Req() req: Request,
+    ) {
+        const urls = files.map(f => `/uploads/market/${f.filename}`);
+        const item = await this.marketService.findOneItem(id);
+        const user = req.user as User;
+        item.images = item.images ? [...item.images, ...urls]: urls;
+        return this.marketService.updateItem(id, { images: item.images }, user);
     }
 
     @UseGuards(JwtAuthGuard)
