e9ae4488187bbc84b9efe3a39cec320a3933df8c
Makav77 <briceminet@hotmail.fr>
2025-05-07 10:36:45 +0200

    feat(frontend): add edit post page

--- frontend/src/components/Events/EditEventPage.tsx
+++ frontend/src/components/Events/EditEventPage.tsx
@@ -3,8 +3,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import EventForm, { EventFormData } from "./EventForm";
 import { getEvent, updateEvent } from "../../api/eventService";
 import { EventDetails } from "../../api/eventService";
+import { toast } from "react-toastify";
 
-export default function EditEventPage() {
+function EditEventPage() {
     const navigate = useNavigate();
     const { id } = useParams<{ id: string}>();
     const [event, setEvent] = useState<EventDetails | null>(null);
@@ -22,6 +23,7 @@ export default function EditEventPage() {
     const handleSubmit = async (data: EventFormData) => {
         if (!id) return;
         const updated = await updateEvent(id, data);
+        toast.success("Event updated !");
         navigate(`/events/${updated.id}`);
     }
 
@@ -29,10 +31,12 @@ export default function EditEventPage() {
         return <p>Loading...</p>
     }
 
-    return(
+    return (
         <div className="p-6">
             <h1 className="w-[28%] mx-auto text-4xl font-semibold mb-4 pl-2">Edit event</h1>
             <EventForm defaultValues={event} onSubmit={handleSubmit} />
         </div>
     );
 }
+
+export default EditEventPage;
