ab7e83efbaf851adfb2773f9ed0025f1b3cf6a73
Makav77 <briceminet@hotmail.fr>
2025-05-07 16:19:34 +0200

    feat(backend): add market entity

---
+++ backend/src/modules/Market/entities/market.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm";
+import { User } from "src/modules/Users/entities/user.entity";
+
+@Entity("market_items")
+export class MarketItem {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ length: 100 })
+    title: string;
+
+    @Column("text")
+    description: string;
+
+    @Column({ type: "decimal", precision: 10, scale: 2, nullable: true })
+    price?: number;
+
+    @Column("text", { array: true, nullable: true })
+    images?: string[];
+
+    @Column({ nullable: true })
+    contactEmail?: string;
+
+    @Column({ nullable: true })
+    contactPhone?: string;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: "timestamp" })
+    updatedAt: Date;
+
+    @ManyToOne(() => User, (user) => user.marketItems, { nullable: false })
+    author: User;
+}
