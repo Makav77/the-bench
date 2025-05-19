448fe02a430a9c95787660087e2e46c0eef1789a
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-05-19 19:43:21 +0200

    feat: add new scaper and models

--- java-client/src/main/java/org/example/scraping/Scraper.java
+++ java-client/src/main/java/org/example/scraping/Scraper.java
@@ -1,6 +1,8 @@
 package org.example.scraping;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import org.example.dao.DayArticlesDAO;
@@ -12,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.example.scraping.Model.Cinema.*;
 
 public class Scraper {
     public static void getNewsParis() {
@@ -75,6 +78,79 @@ public class Scraper {
             DayArticlesUtils.InsertDayArticles(dayArticles);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    public static List<FilmPresentation> getSeancesChatelet(){
+        System.out.println("Seances Chatelet");
+        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        Logger.getLogger("org.apache.http").setLevel(Level.OFF);
+        try (WebClient webClient = new WebClient()) {
+            System.out.println("Seances Chatelet 1");
+
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            webClient.addRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+            System.out.println("Seances Chatelet 2");
+
+            HtmlPage page = webClient.getPage("https://www.ugc.fr/cinema.html?id=10");
+            System.out.println("Seances Chatelet 3");
+
+            webClient.waitForBackgroundJavaScript(5000);
+            System.out.println("Seances Chatelet 4");
+
+            HtmlElement body = page.getBody();
+            //List<HtmlElement> seance = body.getByXPath("//div[@class='slider-item']");
+
+            List<HtmlElement> filmBlocks = body.getByXPath("//div[contains(@class,'component--film-presentation')]");
+            List<FilmPresentation> films = new ArrayList<>();
+            for(HtmlElement filmBlock : filmBlocks) {
+                FilmPresentation film = new FilmPresentation();
+
+                HtmlAnchor titleAnchor = filmBlock.getFirstByXPath(".//div[contains(@class,'block--title')]//a");
+                film.titre = titleAnchor.asNormalizedText();
+                film.lien = titleAnchor.getHrefAttribute();
+
+                HtmlImage img = filmBlock.getFirstByXPath(".//div[contains(@class,'img-wrapper')]//img");
+                film.imageUrl = img.getSrcAttribute();
+
+                HtmlElement genreDurée = filmBlock.getFirstByXPath(".//p[contains(@class,'color--dark-blue')]");
+                String[] parts = genreDurée.asNormalizedText().split("\\(");
+                film.genres = parts[0].trim();
+                film.duree = parts.length > 1 ? parts[1].replace(")", "").trim() : "";
+
+                HtmlElement dateSortie = filmBlock.getFirstByXPath(".//p[contains(.,'Sortie le')]//span");
+                film.dateSortie = (dateSortie != null)? dateSortie.asNormalizedText() : "";
+
+                HtmlElement realisateur = filmBlock.getFirstByXPath(".//p[contains(.,'De')]//span");
+                film.realisateur =  (realisateur != null) ? realisateur.asNormalizedText() : "";
+
+                HtmlElement acteurs = filmBlock.getFirstByXPath(".//p[contains(.,'Avec')]//span");
+                film.acteurs = (acteurs != null) ? acteurs.asNormalizedText(): "";
+
+                HtmlElement synopsis = filmBlock.getFirstByXPath(".//p[contains(.,'Synopsis')]//span");
+                film.synopsis = (synopsis != null) ? synopsis.asNormalizedText() : "";
+
+                List<HtmlElement> seancesHtml = body.getByXPath("//ul[contains(@class,'component--screening-cards')]//li");
+                for (HtmlElement seanceHtml : seancesHtml) {
+                    Seance seance = new Seance();
+                    seance.version = ((HtmlElement) seanceHtml.getFirstByXPath(".//span[contains(@class,'screening-lang')]")).asNormalizedText();
+                    seance.heureDebut = ((HtmlElement) seanceHtml.getFirstByXPath(".//div[contains(@class,'screening-start')]")).asNormalizedText();
+                    seance.heureFin = ((HtmlElement) seanceHtml.getFirstByXPath(".//div[contains(@class,'screening-end')]")).asNormalizedText();
+                    seance.salle = ((HtmlElement) seanceHtml.getFirstByXPath(".//div[contains(@class,'screening-detail')]")).asNormalizedText();
+                    HtmlElement button = seanceHtml.getFirstByXPath(".//button");
+                    seance.lienReservation = button.getAttribute("onclick").replace("javascript:location.href='", "").replace("'", "");
+                    film.seances.add(seance);
+                }
+                System.out.println("filme : " + film.acteurs);
+                System.out.println("filme : " +  film.seances.size());
+                films.add(film);
+            }
+            return films;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
         }
     }
 }
