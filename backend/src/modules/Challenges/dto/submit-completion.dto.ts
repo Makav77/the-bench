f3898580ca470786c43ba6c8e64a6645573d72bd
Makav77 <briceminet@hotmail.fr>
2025-05-24 19:55:00 +0200

    feat(backend): add submit completion dto

---
+++ backend/src/modules/Challenges/dto/submit-completion.dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsString, MaxLength, IsNotEmptyObject, ValidateIf, isNotEmptyObject } from "class-validator";
+
+export class SubmitCompletionDTO {
+    @ValidateIf(o => !o.text)
+    @IsNotEmptyObject({ nullable: true })
+    imageUrl?: string;
+
+    @ValidateIf(o => !o.imageUrl)
+    @IsString()
+    @MaxLength(2000)
+    text?: string;
+}
