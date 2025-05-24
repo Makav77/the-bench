e84f7c457246f9705bac8cb9c95cc856e688a09a
Makav77 <briceminet@hotmail.fr>
2025-05-24 18:38:15 +0200

    feat(backend): add challenge completion entity

---
+++ backend/src/modules/Challenges/entities/challenge-completion.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from "typeorm";
+import { User } from "src/modules/Users/entities/user.entity";
+import { Challenge } from "./challenge.entity";
+
+@Entity({ name: "challenge_completions" })
+export class ChallengeCompletion {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ type: "text", nullable: false })
+    text?: string;
+
+    @Column({ nullable: true })
+    imageUrl?: string;
+
+    @Column({ default: false })
+    validated: boolean;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt: Date;
+
+    // @ManyToOne(() => User, (user) => user.challengeCompletions, { nullable: false })
+    // user: User;
+
+    // @ManyToOne(() => Challenge, (challenge) => challenge.completions, { nullable: false, onDelete: "CASCADE" })
+    // challenge: Challenge;
+}
