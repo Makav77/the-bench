703133c7c7e4bcb87630a9db93870bd1d37fd39f
Makav77 <briceminet@hotmail.fr>
2025-05-24 18:32:29 +0200

    feat(backend): add challenge-registration entoty

---
+++ backend/src/modules/Challenges/entities/challenge-registration.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from "typeorm";
+import { User } from "src/modules/Users/entities/user.entity";
+import { Challenge } from "./challenge.entity";
+
+@Entity({ name: "challenge_registration" })
+export class ChallengeRegistration {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt: Date;
+
+    // @ManyToOne(() => User, (user) => user.challengeRegistrations, { nullable: false })
+    // user: User;
+
+    // @ManyToOne(() => Challenge, (challenge) => challenge.registrations, { nullable: false, onDelete: "CASCADE" })
+    // challenge: Challenge;
+}
