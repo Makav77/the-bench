e257406829636cc1aad179b4cee34d751766ed07
Makav77 <briceminet@hotmail.fr>
2025-05-24 22:20:57 +0200

    feat(backend): update challenge service with update, remove, subscribe, unsubscribe, SubmitCompletion and ValidateCompletion

--- backend/src/modules/Challenges/challenges.service.ts
+++ backend/src/modules/Challenges/challenges.service.ts
@@ -1,16 +1,23 @@
-import { Injectable, NotFoundException, ForbiddenException } from "@nestjs/common";
+import { Injectable, NotFoundException, ForbiddenException, BadRequestException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { MoreThan, Repository } from "typeorm";
+import { MoreThan, Not, Repository } from "typeorm";
 import { Challenge } from "./entities/challenge.entity";
+import { ChallengeRegistration } from "./entities/challenge-registration.entity";
+import { ChallengeCompletion } from "./entities/challenge-completion.entity";
 import { CreateChallengeDTO } from "./dto/create-challenge.dto";
+import { SubmitCompletionDTO } from "./dto/submit-completion.dto";
+import { ValidateCompletionDTO } from "./dto/validate-completion.dto";
 import { User, Role } from "../Users/entities/user.entity";
-
 
 @Injectable()
 export class ChallengesService {
     constructor(
         @InjectRepository(Challenge)
         private readonly challengeRepo: Repository<Challenge>,
+        @InjectRepository(ChallengeRegistration)
+        private readonly registrationRepo: Repository<ChallengeRegistration>,
+        @InjectRepository(ChallengeCompletion)
+        private readonly completionRepo: Repository<ChallengeCompletion>,
     ) {}
 
     async findAllChallenges(page = 1, limit = 10): Promise<{ data: Challenge[]; total: number; page: number; lastPage: number }> {
@@ -20,7 +27,7 @@ export class ChallengesService {
             order: { endDate: "DESC" },
             skip: offset,
             take: limit,
-            relations: ["author", "registrations"],
+            relations: ["author", "registrations", "completions"],
         });
 
         const lastPage = Math.ceil(total / limit);
@@ -30,7 +37,7 @@ export class ChallengesService {
     async findOneChallenge(id: string): Promise<Challenge> {
         const challenge = await this.challengeRepo.findOne({
             where: { id },
-            relations: ["author", "registrations", "completions"],
+            relations: ["author", "registrations", "registrations.user", "completions", "completions.user"],
         });
         if (!challenge) {
             throw new NotFoundException("Challenge not found.");
@@ -39,10 +46,135 @@ export class ChallengesService {
     }
 
     async createChallenge(createChallengeDTO: CreateChallengeDTO, author: User): Promise<Challenge> {
-        const challenge = this.challengeRepo.create({ 
-            ...createChallengeDTO,
+        const { title, description, startDate, endDate, successCriteria } = createChallengeDTO;
+        const challenge = this.challengeRepo.create({
+            title,
+            description,
+            startDate: new Date(startDate),
+            endDate: new Date(endDate),
+            successCriteria,
             author,
         });
         return this.challengeRepo.save(challenge);
+    }
+
+
+    async updateChallenge(id: string, updatedChallengeDTO: CreateChallengeDTO, user: User): Promise<Challenge> {
+        const challenge = await this.challengeRepo.findOne({
+            where: { id },
+            relations: ["author"],
+        });
+
+        if (!challenge) {
+            throw new NotFoundException("Challenge not found.");
+        }
+
+        if (challenge.author.id !== user.id && user.role !== Role.ADMIN) {
+            throw new ForbiddenException("You are not allowed to edit this challenge.");
+        }
+
+        const updated = this.challengeRepo.merge(challenge, updatedChallengeDTO);
+        return this.challengeRepo.save(updated);
+    }
+
+    async removeChallenge(id: string, user: User): Promise<void> {
+        const challenge = await this.challengeRepo.findOne({
+            where: { id },
+            relations: ["author"]
+        });
+
+        if (!challenge) {
+            throw new NotFoundException("Challenge not found.");
+        }
+
+        if (challenge.author.id !== user.id && user.role !== Role.ADMIN) {
+            throw new ForbiddenException("You are not allowed to delete this challenge");
+        }
+
+        await this.challengeRepo.delete(id);
+    }
+
+    async subscribe(id: string, user: User): Promise <void> {
+        const challenge = await this.challengeRepo.findOne({
+            where: { id },
+            relations: ["author"],
+        });
+
+        if (!challenge) {
+            throw new NotFoundException("Challenge not found.");
+        }
+
+        if (challenge.registrations.some(r => r.user.id === user.id)) {
+            throw new BadRequestException("Already registered");
+        }
+
+        const register = this.registrationRepo.create({ challenge: challenge, user });
+        await this.registrationRepo.save(register);
+    }
+
+    async unsubscribe(id: string, user: User): Promise<void> {
+        const register = await this.registrationRepo.findOne({
+            where: {
+                challenge: { id },
+                user: { id: user.id },
+            },
+        });
+
+        if (!register) {
+            throw new NotFoundException("Registration not found");
+        }
+
+        await this.registrationRepo.delete(register.id);
+    }
+
+    async submitCompletion(id: string, submitCompletionDTO: SubmitCompletionDTO, user: User): Promise<ChallengeCompletion> {
+        const challenge = await this.challengeRepo.findOne({
+            where: { id },
+            relations: ["author"],
+        });
+
+        if (!challenge) {
+            throw new NotFoundException("Challenge not found.");
+        }
+
+        if (!challenge?.registrations.some(r => r.user.id === user.id)) {
+            throw new ForbiddenException("You must register first.");
+        }
+
+        const completion = this.completionRepo.create({
+            challenge: challenge,
+            user,
+            text: submitCompletionDTO.text,
+            imageUrl: submitCompletionDTO.imageUrl,
+        });
+
+        return this.completionRepo.save(completion);
+    }
+
+    async validateCompletion(challengeId: string, completionId: string, validateCompletionDTO: ValidateCompletionDTO, user: User): Promise<ChallengeCompletion> {
+        const challenge = await this.challengeRepo.findOne({
+            where: { id: challengeId },
+            relations: ["author"],
+        });
+
+        if (!challenge) {
+            throw new NotFoundException("Challenge not found.");
+        }
+
+        if (challenge.author.id !== user.id && user.role !== Role.ADMIN) {
+            throw new ForbiddenException("You are not allowed to validate the completion.");
+        }
+
+        const completion = await this.completionRepo.findOne({
+            where: { id: completionId },
+            relations: ["user", "challenge"],
+        });
+
+        if (!completion) {
+            throw new NotFoundException("Completion not found.");
+        }
+
+        completion.validated = validateCompletionDTO.validated;
+        return this.completionRepo.save(completion);
     }
 }
