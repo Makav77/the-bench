d0e5c86aea34966ed3b834e748fc2a849492ac86
Makav77 <briceminet@hotmail.fr>
2025-06-07 18:33:44 +0200

    fix(poll and image): rename post by poll and image

--- frontend/src/components/Community/Gallery/GalleryItemDetailPage.tsx
+++ frontend/src/components/Community/Gallery/GalleryItemDetailPage.tsx
@@ -117,7 +117,7 @@ export default function GalleryItemDetailPage() {
                             onClick={() => setShowReportModal(true)}
                             className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer"
                         >
-                            Report post
+                            Report image
                         </button>
 
                         {showReportModal && (
--- frontend/src/components/Community/Polls/PollDetailPage.tsx
+++ frontend/src/components/Community/Polls/PollDetailPage.tsx
@@ -230,7 +230,7 @@ function PollDetailPage() {
                     onClick={() => setShowReportModal(true)}
                     className="mt-4 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer"
                 >
-                    Report post
+                    Report poll
                 </button>
 
                 {showReportModal && (
