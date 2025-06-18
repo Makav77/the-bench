74eb3c8cd51073e507c0d719d1497547d8c2448f
Makav77 <briceminet@hotmail.fr>
2025-06-18 22:21:15 +0200

    feat(backend): add news.service

---
+++ backend/src/modules/News/news.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { News, NewsDocument } from "./news.schema";
+import { CreateNewsDTO } from "./dto/create-news.dto";
+import { UpdateNewsDTO } from "./dto/update-news.dto";
+
+@Injectable()
+export class NewsService {
+    constructor(@InjectModel(News.name) private newsModel: Model<NewsDocument>) {}
+
+    async findAllNews(page = 1, limit = 5): Promise<{ data: News[]; total: number; page: number; lastPage: number; }> {
+        const skip = (page - 1) * limit;
+        const total = await this.newsModel.countDocuments();
+        const data = await this.newsModel
+            .find()
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .exec();
+
+        const lastPage = Math.ceil(total / limit);
+        return { data, total, page, lastPage };
+    }
+
+    async findOneNews(id: string): Promise<News> {
+        const news = await this.newsModel.findById(id).exec();
+        if (!news) {
+            throw new NotFoundException("News not found.");
+        }
+        return news;
+    }
+
+    async createNews(createNewsDTO: CreateNewsDTO): Promise<News> {
+        const createdNews = new this.newsModel(createNewsDTO);
+        return await createdNews.save();
+    }
+
+    async updateNews(id: string, updateNewsDTO: UpdateNewsDTO): Promise<News> {
+        const updatedNews = await this.newsModel.findByIdAndUpdate(id, updateNewsDTO, { new: true }).exec();
+        if (!updatedNews) {
+            throw new NotFoundException("News not found for updating");
+        }
+        return updatedNews;
+    }
+
+    async removeNews(id: string): Promise<void> {
+        const removed = await this.newsModel.findByIdAndDelete(id).exec();
+        if (!removed) {
+            throw new NotFoundException("News not found for removing")
+        }
+    }
+}
