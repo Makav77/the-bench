426dd6ae783628b6e0bf0ffc0424d2f8545ebda3
Makav77 <briceminet@hotmail.fr>
2025-07-08 21:27:30 +0200

    allow cors, move to dev mode and uncomment adminer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,11 @@ import { join } from 'path';
 
 import * as dotenv from 'dotenv';
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://209.38.138.250',
+];
+
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger: ["error", "warn", "log", "debug", "verbose"] });
     const port = 3000;
@@ -15,10 +20,21 @@ async function bootstrap() {
 
     dotenv.config();
 
-    const origin = process.env.NODE_ENV === 'prod' ? process.env.FRONTEND_ORIGIN_PROD : process.env.FRONTEND_ORIGIN_DEV;
+    //const origin = process.env.NODE_ENV === 'prod' ? process.env.FRONTEND_ORIGIN_PROD : process.env.FRONTEND_ORIGIN_DEV;
+
+    // app.enableCors({
+    //     origin: origin,
+    //     credentials: true,
+    // });
 
     app.enableCors({
-        origin: origin,
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         credentials: true,
     });
 
