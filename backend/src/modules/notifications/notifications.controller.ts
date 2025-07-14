e757744d7f52742d59d89b10b5b09c2de597c8ab
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-07-14 18:14:26 +0200

    feat(backend): add guards verification on notifications routes

--- backend/src/modules/notifications/notifications.controller.ts
+++ backend/src/modules/notifications/notifications.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { NotificationsService } from "./notifications.service";
+import { JwtAuthGuard } from "../Auth/guards/jwt-auth.guard";
+
 
 @Controller("notifications")
 export class NotificationsController {
   constructor(private readonly service: NotificationsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(":userId")
   getUserNotifications(@Param("userId") userId: string) {
     return this.service.findByUser(userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   createNotification(
     @Body() dto: { userId: string; title: string; message: string }
@@ -17,16 +21,19 @@ export class NotificationsController {
     return this.service.create(dto.userId, dto.title, dto.message);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put("read/all/:userId")
   markAllAsRead(@Param("userId") userId: string) {
     return this.service.markAllAsRead(userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(":id")
   async deleteNotification(@Param("id") id: string) {
     return this.service.delete(id);
   }
-
+  
+  @UseGuards(JwtAuthGuard)
   @Put(":id/read")
   async updateReadStatus(@Param("id") id: string, @Body("read") read: boolean) {
     return this.service.updateReadStatus(id, read);
